dd1cdb71be90b043de8d9f8e03b9a7a203fb5b4e
Nikhil <nikhilnagarkar9@gmail.com>
2022-11-18 20:03:27 -0800

    Add pseudo LRU access for 4-way

--- cache.sv
+++ cache.sv
@@ -3,14 +3,17 @@ import cache_define ::*;
 
 module cache( input [31:0] address, input [4:0]  command );
 
-
+////////////CACHE///////////////
 //logic [5:0] 	Byte_Select [3:0][1:0];
 //logic [1:0] 	Index_Bits  [3:0];
-logic [23:0]    Tag_Array    [3:0][1:0];
+logic [23:0]    Tag_Array    [3:0][3:0];
+logic [2:0]     LRU_Array    [3:0];
 
+//////ADDRESS BITS/////////
 logic [5:0] 	Byte_Select;
 logic [1:0] 	Index_Bits;
 logic [23:0]    Tag_Bits;
+
 
 int i_point,b_point;
 int status;
@@ -25,7 +28,7 @@ initial begin
 
 for (i_point = 0; i_point< 5 ; i_point++) begin
 
-	for(b_point = 0;b_point < 3; b_point++) begin
+	for(b_point = 0;b_point < 4; b_point++) begin
 
 		Tag_Array[i_point][b_point] = 24'b0;
         end
@@ -35,46 +38,74 @@ end
 
 always@(command or address) begin
 
+$display("%d %d %d", index, byte_bits, tag_bits); 
 	case(command)
 
 		0: begin
 
                         $display("READDD COMMMANDDD");
-
+                         status = 0;
 			
 
-			for(b_point = 0; b_point < 3; b_point++) begin
+			for(b_point = 0; b_point < 4; b_point++) begin
 				
 				if(Tag_Array[Index_Bits][b_point] == Tag_Bits) begin
 
-					$display("It's a FUCKIN HIIITTT!");
-                                        $display("Tag = %b ,Index_bits = %b, Tag_bits = %b" ,Tag_Array[Index_Bits][b_point], Index_Bits, Tag_Bits);
-                                end
-                                else 
+					$display("It's a FUCKIN HITT!");
+					
+
+					case(b_point) 
+					
+						0:  LRU_Array[Index_Bits][1:0] =  2'b00;
+                        			1:  LRU_Array[Index_Bits][1:0] =  2'b10;
+						2:begin  
+							LRU_Array[Index_Bits][0] =  1'b1;
+                                                        LRU_Array[Index_Bits][2] =  1'b0;
+                                                  end
+                                                3:begin  
+							LRU_Array[Index_Bits][0] =  1'b1;
+                                                        LRU_Array[Index_Bits][2] =  1'b1;
+                                                  end
+						
+                    			endcase
+                    			 					
+					$display("Tag = %b ,Index_bits = %b, Tag_bits = %b,LRU %b" ,Tag_Array[Index_Bits][b_point], Index_Bits, Tag_Bits,LRU_Array[Index_Bits]);
 					status = 1;
+                                        break;
+                
+				end
+				
+			end 
+
+			if(status != 1) 
+				status = 2;
 					  
-                        end
-			
-                         if(status) begin
+				
+			if(status == 2) begin
+				
 				status = 0;
 				$display("It's a Miss :( ");
 
-				for(b_point = 0; b_point < 2; b_point++) begin
+				for(b_point = 0; b_point < 4; b_point++) begin
 				
 					if(Tag_Array[Index_Bits][b_point] == 24'b0) begin
 
 						Tag_Array[Index_Bits][b_point] = Tag_Bits;  //Saving to every empty location in set
+                                                
+						break;
 
-                                	end 
+					end 
                          	
-			
-			 	end
+			       $display("%d",Tag_Array[Index_Bits][b_point]);
+			       end
 		
-			end 
+			
 
+		       end
+                      
 		end
 		default:
-			$display("BHOKAT GELA");
+			$display("Jhav Jhav Nusti");
 			
 			
 	endcase
--- cache_define.sv
+++ cache_define.sv
@@ -1,13 +1,13 @@
 package cache_define;
 
 parameter addr_size  = 32;
-parameter cache_size = 16000000;
-parameter assoc      = 8;
+parameter cache_size = 1024;
+parameter assoc      = 4;
 parameter byte_line  = 64;
 
 
 
-parameter index       = $clog2(cache_size/assoc * byte_line);
+parameter index       = $clog2(cache_size/(assoc * byte_line));
 parameter byte_bits   = $clog2(byte_line);
 parameter tag_bits    = addr_size - (index + byte_bits);
 
