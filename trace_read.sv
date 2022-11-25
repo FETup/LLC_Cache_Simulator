7ef9c8f957d5e179995193713d947c6b2affef16
Nikhil <nikhilnagarkar9@gmail.com>
2022-11-24 17:10:04 -0800

    Add delay

--- trace_read.sv
+++ trace_read.sv
@@ -17,49 +17,35 @@ cache DUT(.address(address),.command(command));
 
 initial begin
 
-
-
-if($value$plusargs ("MODE=%d", w_mode))
-begin
-    if( w_mode == NORMAL_MODE)
-	$display ("Mode: NORMAL_MODE");
-    else
-	$display ("Mode: SILENT_MODE");
-
-end
-
-file = $fopen("./ECE585_Final_Project/trace.txt","r");
-
-if(file) begin
- $display("File Opened");
- while(!$feof(file))
+	if($value$plusargs ("MODE=%d", w_mode))
 	begin
-	$fgets(line,file);
-	#20
-	$sscanf(line,"%d %h",command,address);
-        #20
-	$display("Command = %d Address = %h",command,address);
-      
-/*
-	case(command)
-	  L1_DATA_REQ_R  :$display("Command: L1_DATA_REQ_R"); 
-	  L1_DATA_REQ_W  :$display("Command: L1_DATA_REQ_W ");
-	  L1_INST_REQ_R  :$display("Command: L1_INST_REQ_R "); 
-	  SNOOP_INVAL    :$display("Command: SNOOP_INVAL   ");
-	  SNOOP_REQ_R    :$display("Command: SNOOP_REQ_R   ");
-	  SNOOP_REQ_W	 :$display("Command: SNOOP_REQ_W   ");
-	  SNOOP_MODREQ_R :$display("Command: SNOOP_MODREQ_R");
-	  RESET		 :$display("Command: RESET");
-	  PRINT		 :$display("Command: PRINT");
-	endcase
-*/
+    		if( w_mode == NORMAL_MODE)
+			$display ("Mode: NORMAL_MODE");
+    		else
+			$display ("Mode: SILENT_MODE");
+
+	end
+
+	file = $fopen("./ECE585_Final_Project/trace.txt","r");
+
+	if(file) begin
+ 		$display("File Opened");
+
+ 		while(!$feof(file))
+		begin
+			$fgets(line,file);
+			#50;
+			$sscanf(line,"%d %h",command,address);
+        		#50;
+	//$display("Command = %d Address = %h",command,address);
+       	
 
 
-end
+		end
 
-end
-else
- $display("File: trace.txt\tStatus: Fail");
+	end
+	else
+ 		$display("File: trace.txt\tStatus: Fail");
 
 
 
